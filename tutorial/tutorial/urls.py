f716066f205bd283eb39d977547e7001c22e4b80
angmar33 <paultsvilyuk@gmail.com>
2019-04-03 15:25:15 +0300

    schema_view added to urls.

--- tutorial/tutorial/urls.py
+++ tutorial/tutorial/urls.py
@@ -15,9 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.schemas import get_schema_view
+# from rest_framework.documentation import include_docs_urls
+
+schema_view = get_schema_view(title='Pastebin API')
 
 urlpatterns = [
+    path('schema/', schema_view),
     path('admin/', admin.site.urls),
     path('', include('snippets.urls')),
     path('api-auth/', include('rest_framework.urls')),
+    # path('docs', include_docs_urls(title='API documentation'))
 ]
